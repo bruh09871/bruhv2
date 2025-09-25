13e4210fd88f0bc658b8d7aa0b5801fce280b619
bruh09871 <147663142+bruh09871@users.noreply.github.com>
2025-09-25 19:11:01 -0400

    Update hyperbeam.js

--- api/hyperbeam.js
+++ api/hyperbeam.js
@@ -1,27 +1,19 @@
-// Launch VM
 export default async function handler(req, res) {
   try {
-    const { shared } = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
-    
     const response = await fetch("https://engine.hyperbeam.com/v0/vm", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer sk_test_3yMikIFj_7Gm-r9FSjAv0PrAR96DucD7z3vqhn4wsgI`,
+        "Authorization": `Bearer sk_test_5ECQZME56sa9OHlk4tGJQyFDrLfKObI-MaER414644w`,
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({
-        start_url: "https://google.com",
-        collaborative: shared || false
-      })
+      body: JSON.stringify({ start_url: "https://google.com" })
     });
 
     const data = await response.json();
-    // Return embed_url + vm_id so frontend can destroy/reset
     res.status(response.status).json({
       embed_url: data.embed_url,
       vm_id: data.id
     });
-
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
