00ac42a11b5e836a68ec0e8db5628cf7440c1ff7
bruh09871 <147663142+bruh09871@users.noreply.github.com>
2025-09-25 18:48:23 -0400

    Update hyperbeam.js

--- api/hyperbeam.js
+++ api/hyperbeam.js
@@ -22,9 +22,9 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-    res.status(response.status).json(data);
+    // Send both embed URL and VM ID back
+    res.status(response.status).json({ embed_url: data.embed_url, vm_id: data.id });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 }
-
