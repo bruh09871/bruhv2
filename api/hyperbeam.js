361ab79a88de64e33a5d8ca725fb2f8ee37a0be6
bruh09871 <147663142+bruh09871@users.noreply.github.com>
2025-09-25 19:05:29 -0400

    Update hyperbeam.js

--- api/hyperbeam.js
+++ api/hyperbeam.js
@@ -1,28 +1,27 @@
+// Launch VM
 export default async function handler(req, res) {
   try {
-    let shared = false;
-    if (req.body) {
-      if (typeof req.body === "string") {
-        shared = JSON.parse(req.body).shared || false;
-      } else if (typeof req.body === "object") {
-        shared = req.body.shared || false;
-      }
-    }
-
+    const { shared } = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+    
     const response = await fetch("https://engine.hyperbeam.com/v0/vm", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${process.env.HYPERBEAM_KEY}`,
+        "Authorization": `Bearer sk_test_5ECQZME56sa9OHlk4tGJQyFDrLfKObI-MaER414644w`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
         start_url: "https://google.com",
-        collaborative: shared
+        collaborative: shared || false
       })
     });
 
     const data = await response.json();
-    res.status(response.status).json({ embed_url: data.embed_url, vm_id: data.id });
+    // Return embed_url + vm_id so frontend can destroy/reset
+    res.status(response.status).json({
+      embed_url: data.embed_url,
+      vm_id: data.id
+    });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
