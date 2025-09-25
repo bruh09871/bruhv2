b6c5cdf632035aa1f0079fcf8bc7067a35d718ee
bruh09871 <147663142+bruh09871@users.noreply.github.com>
2025-09-25 19:11:15 -0400

    Update hyperbeam_destroy.js

--- api/hyperbeam_destroy.js
+++ api/hyperbeam_destroy.js
@@ -1,4 +1,3 @@
-// Destroy VM
 export default async function handler(req, res) {
   try {
     const { vm_id } = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
@@ -7,18 +6,16 @@ export default async function handler(req, res) {
     const response = await fetch(`https://engine.hyperbeam.com/v0/vm/${vm_id}`, {
       method: "DELETE",
       headers: {
-        "Authorization": `Bearer sk_test_3yMikIFj_7Gm-r9FSjAv0PrAR96DucD7z3vqhn4wsgI`,
+        "Authorization": `Bearer sk_test_5ECQZME56sa9OHlk4tGJQyFDrLfKObI-MaER414644w`,
         "Content-Type": "application/json"
       }
     });
 
-    if (response.ok) {
-      res.status(200).json({ success: true });
-    } else {
+    if (response.ok) res.status(200).json({ success: true });
+    else {
       const data = await response.json();
       res.status(response.status).json(data);
     }
-
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
